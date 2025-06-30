d3981b52f3db97a9154147f8fab37da326e55454
Heruvim6w <salkov_a@mail.ru>
2025-06-30 22:05:09 +0300

    create moonshine author resource

---
+++ app/MoonShine/Resources/AuthorResource.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MoonShine\Resources;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Author;
+
+use MoonShine\Laravel\Resources\ModelResource;
+use MoonShine\UI\Components\Layout\Box;
+use MoonShine\UI\Fields\ID;
+use MoonShine\Contracts\UI\FieldContract;
+use MoonShine\Contracts\UI\ComponentContract;
+use MoonShine\UI\Fields\Text;
+
+/**
+ * @extends ModelResource<Author>
+ */
+class AuthorResource extends ModelResource
+{
+    protected string $model = Author::class;
+
+    protected string $title = 'Authors';
+
+    /**
+     * @return list<FieldContract>
+     */
+    protected function indexFields(): iterable
+    {
+        return [
+            ID::make()->sortable(),
+            Text::make('Name')->sortable(),
+            Text::make('Surname')->sortable(),
+        ];
+    }
+
+    /**
+     * @return list<ComponentContract|FieldContract>
+     */
+    protected function formFields(): iterable
+    {
+        return [
+            Box::make([
+                ID::make(),
+                Text::make('Name')->sortable(),
+                Text::make('Surname')->sortable(),
+                Text::make('Biography')->sortable(),
+            ])
+        ];
+    }
+
+    /**
+     * @return list<FieldContract>
+     */
+    protected function detailFields(): iterable
+    {
+        return [
+            ID::make(),
+        ];
+    }
+
+    /**
+     * @param Author $item
+     *
+     * @return array<string, string[]|string>
+     * @see https://laravel.com/docs/validation#available-validation-rules
+     */
+    protected function rules(mixed $item): array
+    {
+        return [];
+    }
+}
