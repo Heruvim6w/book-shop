7f3911e736180e4bb9de57d73e80a1f8372cfac6
Heruvim6w <salkov_a@mail.ru>
2025-08-20 21:54:38 +0300

    Занялись меню и корзиной

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use App\Models\Author;
 use App\Models\Book;
-use Illuminate\Http\Request;
+use App\Models\Cart;
+use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller
 {
@@ -13,7 +13,21 @@ class IndexController extends Controller
     {
         $books = Book::existBooks()->get();
         $authors = Author::whoseBooksExist()->get();
+        $cart = $this->getCart();
 
-        return view('index', compact('books', 'authors'));
+        return view('index', compact(
+            'books',
+            'authors',
+            'cart'
+        ));
+    }
+
+    private function getCart(): ?Cart
+    {
+        if (Auth::user() && Auth::user()->cart && !Auth::user()->cart->isEmpty()) {
+            return Auth::user()->cart;
+        }
+
+        return null;
     }
 }
